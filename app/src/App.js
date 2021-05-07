abdb42349454240a2f7b40183d0cd10259d81705
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2021-05-07 21:05:18 +0200

    Ejercicio 4

--- app/src/App.js
+++ app/src/App.js
@@ -30,17 +30,18 @@ class App extends PureComponent {
 
 	getTeams(teams) {
 		const teamsTemp = {};
-		teams.forEach(
-			(team) => (teamsTemp[team.id] = team)
-		);
+		teams.forEach((team) => (teamsTemp[team.id] = team));
 		return teamsTemp;
 	}
-
+  
 	render() {
 		const {players, teams} = this.state;
 		return (
 			<div className="App">
-					<h3>Los Jugadores</h3>
+				<header className="App-header">
+					<button className="App-pichichis-button">Pichichis</button>
+				</header>
+				<h3>Los Jugadores</h3>
 				<div className="App-players">
 					{/* 
           TODO ejercicio 2
@@ -49,35 +50,39 @@ class App extends PureComponent {
           y una lista con sus nombres. 
           ** Los comentarios de los ejercicios no los borres.
         */}
-						{/* 
+					{/* 
             TODO ejercicio 3
             Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
             Guiate por las imágenes.
            */}
-						{players.map((player) => (
-							<div className="App-player" key={player.id}>
-								<div className="App-player-content">
-									<div className="App-player-image App-flex">
-										<img src={player.img}></img>
-									</div>
-									<div className="App-player-text">
-										<span className="App-player-name">
-											{player.name}
-										</span>
-										<span className="App-player-position">
-											{player.position}
-										</span>
+					{players.map((player) => (
+						<div className="App-player" key={player.name}>
+							<div className="App-player-content">
+								<div className="App-player-image App-flex">
+									<img src={player.img}></img>
+								</div>
+								<div className="App-player-text">
+									<span className="App-player-name">
+										{player.name}
+									</span>
+									<span className="App-player-position">
+										{player.position}
+									</span>
+									{teams[player.teamId] && (
 										<span className="App-player-team">
 											{teams[player.teamId].name}
 										</span>
-									</div>
+									)}
 								</div>
+							</div>
+							{teams[player.teamId] && (
 								<img
 									className="App-team-shield"
 									src={`https:${teams[player.teamId].shield}`}
 								></img>
-							</div>
-						))}
+							)}
+						</div>
+					))}
 				</div>
 			</div>
 		);
